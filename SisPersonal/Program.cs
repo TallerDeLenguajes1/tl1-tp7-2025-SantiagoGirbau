bdff55d16ee89dc840a4e94800408a2c7f1017a0
SantiagoGirbau <santiagogirbau@gmail.com>
2025-06-03 00:41:24 -0300

    Apartados a i, ii, iii y b i, ii, iii resueltos (perdon)

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SisPersonal;
 
-Empleado Santiago = new Empleado("Mariano", "Girbau Alfaro");
-Console.WriteLine($"Nombre y apellido: {Santiago.Nombre} {Santiago.Apellido}");
-Santiago.Nombre= "Santiago";
-Santiago.Apellido= "Girbau";
-Console.WriteLine($"Nombre y apellido: {Santiago.Nombre} {Santiago.Apellido}");
+Empleado Juan = new Empleado("Juan", "Girbau", new DateTime(1980, 8, 3), 'c', new DateTime(1998, 5, 3), 650000, Empleado.Cargo.Ingeniero);
+Console.WriteLine($"Nombre y apellido: {Juan.Nombre} {Juan.Apellido}");
+int edadDelEmpleado = Juan.Edad();
+Console.WriteLine(edadDelEmpleado);
+Console.WriteLine(Juan.Jubilarse());
+Console.WriteLine(Juan.Antiguedad());
+Console.WriteLine(Juan.salarioFinal());
--- SisPersonal/SisPersonal.cs
+++ SisPersonal/SisPersonal.cs
@@ -1,24 +1,95 @@
 namespace SisPersonal{
-    //using time;
-    public class Empleado{
 
-        public Empleado(string nombreIngresado, string apellidoIngresado){
-            this.nombre = nombreIngresado;
-            this.apellido = apellidoIngresado;
-        }
+    public class Empleado
+    {
+
 
         private string nombre;
         private string apellido;
-        //private datetime fechaDeNacimiento;
+        private DateTime fechaDeNacimiento;
         private char estadoCivil;
-        //private datetime ingreso;
+        private DateTime ingreso;
         private double sueldoBasico;
-        //private cargos cargo;
 
-        public string Nombre {get => nombre; set => nombre = value;}
-        public string Apellido {get => apellido; set => apellido = value;}
+        public enum Cargo
+        {
+            Administrativo = 0, Ingeniero = 1, Especialista = 2, Investigador = 3
+        };
+        private Cargo cargo;
 
-        //Empleado();
+        public Empleado(string nombreIngresado, string apellidoIngresado, DateTime fechaNac, char estadoCivilIngresado, DateTime fechaDeIngreso, double sueldo, Cargo cargoIngresado)
+        {
+            this.nombre = nombreIngresado;
+            this.apellido = apellidoIngresado;
+            this.fechaDeNacimiento = fechaNac;
+            this.estadoCivil = estadoCivilIngresado;
+            this.ingreso = fechaDeIngreso;
+            this.sueldoBasico = sueldo;
+            this.cargo = cargoIngresado;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+
+        public int Edad()
+        {
+            if (System.DateTime.Today.Month > fechaDeNacimiento.Month)
+            {
+                return System.DateTime.Today.Year - fechaDeNacimiento.Year + 1;
+            }
+            if (System.DateTime.Today.Month == fechaDeNacimiento.Month && System.DateTime.Today.Day >= fechaDeNacimiento.Day)
+            {
+                return System.DateTime.Today.Year - fechaDeNacimiento.Year + 1;
+            }
+            return System.DateTime.Today.Year - fechaDeNacimiento.Year;
+        }
+
+        public int Jubilarse()
+        {
+            return 65 - Edad();
+        }
+
+        public int Antiguedad()
+        {
+            if (System.DateTime.Today.Month > ingreso.Month)
+            {
+                return System.DateTime.Today.Year - ingreso.Year + 1;
+            }
+            if (System.DateTime.Today.Month == ingreso.Month && System.DateTime.Today.Day >= ingreso.Day)
+            {
+                return System.DateTime.Today.Year - ingreso.Year + 1;
+            }
+            return System.DateTime.Today.Year - ingreso.Year;
+        }
+
+        public double salarioFinal()
+        {
+            int antiguedad = Antiguedad();
+            double adicional;
+            if (antiguedad < 20)
+            {
+                adicional = 0.01 * antiguedad;
+            }
+            else
+            {
+                adicional = 0.25;
+            }
+
+                Console.WriteLine(adicional);
+            if (cargo == Cargo.Ingeniero || cargo == Cargo.Especialista)
+            {
+                adicional *= 1.5;
+            }
+                Console.WriteLine(adicional);
+            if (estadoCivil == 'c' || estadoCivil == 'C')
+            {
+                return sueldoBasico * (1+adicional) + 150000;
+            }
+            else
+            {
+                return sueldoBasico * (1+adicional);
+            }
+        }
     }
 
 
